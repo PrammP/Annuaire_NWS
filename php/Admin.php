bc3924def3bda937fc2c32cb308fa59682bd3260
PrammP <92103872+PrammP@users.noreply.github.com>
2022-11-03 23:39:23 +0100

    Revert "up"
    This reverts commit a65447e3e21e9721fac876f5c7817107997dfdbd.

--- php/Admin.php
+++ php/Admin.php
@@ -17,7 +17,6 @@
     </tr>
     <form method="GET">
 <input type="search" id="myInput" name="s" placeholder="Rechercher un nom.." autocomplete="off">
-
 </form>
 <?php
 
@@ -46,50 +45,52 @@ include "update.php";
         
         <td>$nom</td>
         <td>$prenom</td>
-        <td>$mail</td>";
-
-        switch ($filiere) {
-
-            case "Developpement Web":
-                print "<td><a href='Admin.php?id_fil1=".$id."' id='update'>$filiere</a></td>";
-                break;
-            case "Communication - Graphique":
-                print "<td><a href='Admin.php?id_fil2=".$id."' id='update'>$filiere</a></td>";
-                break;
-            case "Social Media Management":
-                print "<td><a href='Admin.php?id_fil3=".$id."' id='update'>$filiere</a></td>";
-                break;
-            case "E-Business":
-                print "<td><a href='Admin.php?id_fil4=".$id."' id='update'>$filiere</a></td>";
-                break;
-            case "Ne sais pas":
-                print "<td><a href='Admin.php?id_fil5=".$id."' id='update'>$filiere</a></td>";
-                break;
-        };
-        switch ($Annee) {
-
-            case "1":
-                print "<td><a href='Admin.php?id_annee1=".$id."' id='update'>$Annee</a></td>";
-                break;
-            case "2":
-                print "<td><a href='Admin.php?id_annee2=".$id."' id='update'>$Annee</a></td>";
-                break;
-            case "3":
-                print "<td><a href='Admin.php?id_annee3=".$id."' id='update'>$Annee</a></td>";
-                break;
-        }
-         print "
+        <td>$mail</td>
+        <td>$filiere</td>
+        <td>$Annee</td>
         <td>$ville</td>
         <td>$adresse</td>
         <td>$region</td>
+<<<<<<< HEAD
         <td><a href='Admin.php?id_supp=".$id."' id='delete'>Supprimer</a></td>"
 
+=======
+        <td><a href='Admin.php?id_supp=".$id."' id='delete'>Supprimer</a></td>
+        <td><a href='Admin.php?id_edit=".$id."' id='update'>Editer</a> </td>"
+       
+>>>>>>> parent of a65447e... up
         ;
     };
     
  
 
+<<<<<<< HEAD
  
+=======
+ $dbb = null;
+
+ $tri = "SELECT * from `eleve`";
+ if(isset($_GET['s']) AND !empty($_GET['s'])){
+ $recherche = htmlspecialchars($_GET['s']);
+ $tri = $newBD->query('SELECT * from `eleve` WHERE Prenom LIKE "%'.$recherche.'%" ORDER BY eleve_id DESC');
+ print 
+
+ "
+        <tr> 
+        
+        <td>$nom</td>
+        <td>$prenom</td>
+        <td>$mail</td>
+        <td>$filiere</td>
+        <td>$Annee</td>
+        <td>$ville</td>
+        <td>$adresse</td>
+        <td>$region</td>
+        <td><a href='Admin.php?id_supp=".$id."' id='delete'>Supprimer</a></td>
+        <td><a href='Admin.php?id_edit=".$id."' id='update'>Editer</a> </td>"
+
+ }
+>>>>>>> parent of a65447e... up
 ?>
 </table>
 <button class="ReturnAnnu"> 
--- php/update.php
+++ php/update.php
@@ -1,62 +1,18 @@
 <?php
 
-include "database.php";
-
-if(isset($_GET['id_annee1'])){
-    $ID=$_GET['id_annee1'];
-    $UP="UPDATE eleve SET Annee = '2' WHERE eleve_id = $ID";
-    $newBD->prepare($UP)->execute(); 
-    header("Location:Admin.php");
-} 
-
-if(isset($_GET['id_annee2'])){
-    $ID=$_GET['id_annee2'];
-    $UP="UPDATE eleve SET Annee = '3' WHERE eleve_id = $ID";
-    $newBD->prepare($UP)->execute(); 
-    header("Location:Admin.php");
+function Update ($prenom, $nom, $mail, $ID)
+{
+$sql = "UPDATE eleve SET Prenom = ?, Nom = ?, Mail = ? WHERE eleve_id= $ID";
+$newBD = prepare($sql) -> execute([$prenom, $nom, $mail, $ID]);
 }
 
-if(isset($_GET['id_annee3'])){
-    $ID=$_GET['id_annee3'];
-    $UP="UPDATE eleve SET Annee = '1' WHERE eleve_id = $ID";
-    $newBD->prepare($UP)->execute(); 
-    header("Location:Admin.php");
-    
-}
+if(isset($_POST['update']))
+    {
+        $id = $_GET['ID'];
+        $prenom = $_POST['Prenom'];
+        $nom = $_POST['Nom'];
+        $mail = $_POST['Mail'];
+        
 
-
-if(isset($_GET['id_fil1'])){
-    $ID=$_GET['id_fil1'];
-    $UP="UPDATE eleve SET filiere = 'Communication - Graphique' WHERE eleve_id = $ID";
-    $newBD->prepare($UP)->execute(); 
-    header("Location:Admin.php");
-} 
-
-if(isset($_GET['id_fil2'])){
-    $ID=$_GET['id_fil2'];
-    $UP="UPDATE eleve SET filiere = 'Social Media Management' WHERE eleve_id = $ID";
-    $newBD->prepare($UP)->execute(); 
-    header("Location:Admin.php");
-}
-
-if(isset($_GET['id_fil3'])){
-    $ID=$_GET['id_fil3'];
-    $UP="UPDATE eleve SET filiere = 'E-Business' WHERE eleve_id = $ID";
-    $newBD->prepare($UP)->execute(); 
-    header("Location:Admin.php");
-    
-}
-if(isset($_GET['id_fil5'])){
-    $ID=$_GET['id_fil5'];
-    $UP="UPDATE eleve SET filiere = 'Developpement Web' WHERE eleve_id = $ID";
-    $newBD->prepare($UP)->execute(); 
-    header("Location:Admin.php");
-    
-}
-if(isset($_GET['id_fil4'])){
-    $ID=$_GET['id_fil4'];
-    $UP="UPDATE eleve SET filiere = 'Ne sais pas' WHERE eleve_id = $ID";
-    $newBD->prepare($UP)->execute(); 
-    header("Location:Admin.php");
-    
-}
+        Update($prenom, $nom, $mail, $ID);
+    }
